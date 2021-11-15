2091038cbed9bacabb490406b94b04ceb6ff52c6
kimchanhoo <cksgn0709@gmail.com>
2021-11-16 02:33:47 +0900

    First Commit

---
+++ PhotoCard/PhotoCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoCard
+//
+//  Created by Hoo's MacBookAir on 2021/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
